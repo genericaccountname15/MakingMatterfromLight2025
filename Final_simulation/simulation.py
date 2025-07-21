1f4b0c8b1812170d0a2136704a98000c67be98bc
timothy <Timothy.chew22@imperial.ac.uk>
2025-07-21 09:35:35 +0100

    moved hit counter to another module for organisation

--- Final_simulation/simulation.py
+++ Final_simulation/simulation.py
@@ -488,220 +488,6 @@ class Visualiser(Simulation):
             bool: bath visualiser boolean
         """
         return self.bath_vis
-    
-
-class Hit_counter(Simulation):
-    def __init__(self, xray_bath, gamma_pulse, n_samples_angular=400, n_samples=10, n_samples_azimuthal = 1):
-        super().__init__(xray_bath, gamma_pulse, n_samples_angular, n_samples)
-        self.n_samples_azimuthal = n_samples_azimuthal
-
-    ############ METHODS #####################################################################################
-    def count_hits(self, delay):
-        #delay to distance in mm
-        x0 = -delay * 1e-12 * 3e8 * 1e3
-        self.gamma_pulse.set_x_pos(x0)
-
-        #azimuthal angles to sweep
-        max_angle = np.arctan( self.get_gamma_pulse().get_height() / 
-                              self.get_gamma_pulse().get_off_axis_dist() )
-        angles_azim = np.linspace(0, max_angle, self.get_n_samples_azimuthal())
-
-        total_hit_count = 0
-        total_hit_coords = []
-
-
-        for phi in angles_azim:
-            hit_count = 0
-            hit_coords = np.array([])
-            # calculate 'effective gamma pulse parameters' ##########
-            eff_height = self.calc_effective_height(
-                r = self.get_gamma_pulse().get_height(),
-                phi = phi,
-                d = self.get_gamma_pulse().get_off_axis_dist()
-            )
-
-            eff_d = self.calc_effective_d(
-                phi = phi,
-                d = self.get_gamma_pulse().get_off_axis_dist()
-            )
-            hit_count, hit_coords = self.find_hits(eff_height = eff_height, eff_d = eff_d)   
-            
-            if hit_count != 0:
-                total_hit_count += hit_count
-                if len(total_hit_coords) == 0:
-                    total_hit_coords = hit_coords
-                else:
-                    total_hit_coords = np.append(total_hit_coords, hit_coords, axis=0)
-
-
-        return total_hit_count, total_hit_coords
-    
-
-    def calc_effective_height(self, r, phi, d):
-        """
-        Solves a geometric problem to find the new effective height of the 
-        gamma pulse when looking at an azimuthal plane of the x-ray bath.
-        When phi = 0 the effective height is the radius of the beam
-
-        Args:
-            r (float): radius of gamma pulse (mm)
-            phi (float): azimuthal angle being considered (radians)
-            d (float): off-axial displacement (mm)
-
-        Returns:
-            float: new effective height
-        """
-        dist = r
-
-        if phi != 0:
-            m = abs( np.tan(phi) ) #gradient of line, direction don't matter cuz symmetric
-            if d * d - ( 1 + m * m) * ( d * d - r * r ) < 0:
-                pass # ignore not intersecting
-            else:
-                x1 = ( d + np.sqrt( d * d 
-                                        - ( 1 + m * m) * ( d * d - r * r ))
-                                        ) / ( 1 + m * m )
-                y1 = m * x1
-
-                x2 = d
-                y2 = m * d
-
-                dist = np.sqrt( ( x1 - x2 ) ** 2 + ( y1 - y2 ) ** 2 )
-        
-        return dist
-    
-    def calc_effective_d(self, phi, d):
-        """Solves geometric problem to calculate effective off-axis
-        displacement when looking at another azimuthal plane paramaterised
-        by phi
-
-        Args:
-            phi (float): azimuthal angle being considered (radians)
-            d (float): off-axial displacement (mm)
-
-        Returns:
-            float: effective off-axis displacement
-        """
-        eff_d = np.sqrt(
-            d ** 2 + 
-            d * np.tan(phi)
-        )
-
-        return eff_d
-    
-    def est_npairs(self, angles):
-        """Estimates the number of positron pairs produced
-        and lands on the CsI detector
-
-        Returns:
-            _type_: _description_
-        """
-        import values as values
-        from cross_section import c_BW
-        
-        # calculate cross section of each hit and sum #####################################
-        cs_list = []
-        for angle in angles:
-            #get cross section
-            s = 2 * ( 1 - np.cos(angle) ) * 230 * 1.38e-3
-            cs = c_BW(np.sqrt(s)) #get cross sec for 230MeV root s
-            cs *= 1e-28 #convert from barns to m^2
-            cs_list.append(cs)
-
-        # get maximum azimuthal angle we calculate ######################################
-        max_angle = np.arctan( self.get_gamma_pulse().get_height() / 
-                        self.get_gamma_pulse().get_off_axis_dist() )
-        
-        # estimate number of positrons #################################################
-        N_pos = ( values.xray_number_density * values.gamma_photons_number
-                  * values.AMS_transmision * sum(cs_list) 
-                  / self.get_n_samples_azimuthal()
-                  / self.get_n_samples_angular()
-                  / self.get_n_samples()
-                  * ( max_angle / np.pi ) )
-        
-        # estimate uncertainty ##########################################################
-        uncertainty = np.sqrt(
-        (values.gamma_photons_number_err/values.gamma_photons_number) ** 2
-        + (values.xray_number_density_err/values.xray_number_density) ** 2
-        + (values.AMS_transmision_err/values.AMS_transmision) ** 2
-        + (values.gamma_length_err / values.gamma_length) ** 2
-        ) * N_pos
-        
-        return np.array([N_pos, uncertainty])
-
-    def plot_hit_count(self, min_delay, max_delay, samples=50, show_exp_value=False):
-        # set up figure ############################################
-        fig, ax = plt.subplots() #pylint: disable=unused-variable
-        ax.set_title('Hit count against time delay')
-        ax.set_xlabel('Delay (ps)')
-        ax.set_ylabel('Number of hits')
-
-        twin = ax.twinx()
-        twin.set_ylabel('Number of positrons/pC incident on CsI')
-
-
-        # generate values #########################################
-        delay_list = np.linspace(min_delay, max_delay, samples)
-
-        N_pos_list = []
-        hit_count_list = []
-
-        # progress bar #########################
-        prog = 0
-        print('#' * prog + '-' * ( len(delay_list) - prog ) + f'{ prog / len(delay_list) * 100 :.1f}%')
-
-        for delay in delay_list:
-            hit_count, hit_coords = self.count_hits(delay)
-            N_pos_list.append( self.est_npairs(angles = hit_coords[:, 3]) )
-            hit_count_list.append(hit_count)
-
-            self.xray_bath.resample() #resample x-ray distribution
-
-            # progress bar updates #####################################
-            prog += 1
-            print('#' * prog + '-' * ( len(delay_list) - prog ) + f'{ prog / len(delay_list) * 100 :.1f}%')
-        N_pos_list = np.array(N_pos_list)
-
-        
-        # plot values ##################################################
-        hits, = ax.plot(delay_list, hit_count_list, '-x', 
-                        label='Hit count', color='red')
-
-        positrons, = twin.plot(delay_list, N_pos_list[:,0,0], '-o', 
-                               label='Number of positrons/pC incident on CsI', color='blue')
-        fill_band = twin.fill_between(delay_list, N_pos_list[:,0,0] - N_pos_list[:,1,0], N_pos_list[:,0,0] + N_pos_list[:,1,0], 
-                                      color='blue', alpha=0.3, label='Uncertainty')
-
-        if show_exp_value:
-            import values as values
-            exp_value = ax.axvline(x = values.delay_experiment,
-                                    ymin = 0, ymax = 1,
-                                    label = 'Delay used in 2018', color = 'orange')
-            ax.legend(handles=[hits, positrons, fill_band, exp_value])
-        else:
-            ax.legend(handles=[hits, positrons, fill_band])
-        ax.grid()
-
-        plt.show()
-
-
-    def plot_ang_dist(self, delay):
-        _, hit_coords = self.count_hits(delay)
-        angles = hit_coords[:,3]
-        #angles = self.get_xray_bath().get_xray_coords()[:,2]
-
-        plt.title('Angular distribution of hits')
-        plt.xlabel('Angle')
-        plt.ylabel('Quantity')
-        plt.hist(angles, bins=50)
-        plt.show()
-        
-
-    ############ ACCESS METHODS ##############################################################################
-    def get_n_samples_azimuthal(self):
-        return self.n_samples_azimuthal
-        
 
 
 class Test:
@@ -765,6 +551,7 @@ class Test:
         Runs hit counter on experimental values
         """
         import values as values
+        from hit_counter import Hit_counter
         xray = Xray(
             FWHM = values.xray_FWHM,
             rotation = 40 * np.pi / 180
@@ -793,6 +580,7 @@ class Test:
         """
         Checks angular distribution
         """
+        from hit_counter import Hit_counter
         xray = Xray(
             FWHM = 10,
             rotation = 0
@@ -816,6 +604,7 @@ class Test:
         )
 
     def test_hit_reg(self):
+        from hit_counter import Hit_counter
         xray = Xray(
             FWHM = 10,
             rotation = 0
