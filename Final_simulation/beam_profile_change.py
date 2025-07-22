8a4b21d5104a02d66032d60002923e8853d72d4c
timothy <Timothy.chew22@imperial.ac.uk>
2025-07-22 16:32:28 +0100

    optimising shielding

---
+++ Final_simulation/beam_profile_change.py
@@ -0,0 +1,351 @@
+"""
+Changing beam profile
+Introduce a parameter f which labels the distance of the tungsten
+shielding from the centre of the beam
+
+Timothy Chew
+22/07/25
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from simulation import Simulation
+
+class Hit_counter(Simulation):
+    """Counts the number of collisions between the X-ray bath
+    and Gamma pulse
+
+    Attributes:
+        n_samples_azimuthal (float): number of azimuthal samples to take for width of Xray pulse
+    
+    Methods:
+        count_hits: Counts the total number of collisions for a given pulse timing
+        calc_effective_height: Solves a geometric problem to find the new effective height of the 
+                                gamma pulse when looking at an azimuthal plane of the x-ray bath.
+        calc_effective_d: Solves geometric problem to calculate effective off-axis
+                            displacement when looking at another azimuthal plane paramaterised by phi
+        est_npairs: Estimates the number of positron pairs produced and lands on the CsI detector
+        plot_hit_count: Plots the hit count and estimated number of pairs for a range of delays
+                        (with option to save data)
+    """
+    def __init__(self, xray_bath, gamma_pulse, f, n_samples_angular=400, n_samples=10, n_samples_azimuthal = 1):
+        super().__init__(xray_bath, gamma_pulse, n_samples_angular, n_samples)
+        self.n_samples_azimuthal = n_samples_azimuthal
+        self.f = f
+
+    ############ METHODS #####################################################################################
+    def count_hits(self, delay):
+        """Counts the total number of collisions for a given pulse timing
+
+        Args:
+            delay (float): time delay of gamma pulse to x-ray ignition (ps)
+
+        Returns:
+            tuple (float, numpy.ndarray): number of collisions, 
+                                        array of coordinates for each hit (x, y, angles)
+
+        """
+        #delay to distance in mm
+        x0 = -delay * 1e-12 * 3e8 * 1e3
+        self.gamma_pulse.set_x_pos(x0)
+
+        #azimuthal angles to sweep
+        max_angle = np.arctan( self.get_gamma_pulse().get_height() / 
+                              self.get_gamma_pulse().get_off_axis_dist() )
+        angles_azim = np.linspace(0, max_angle, self.get_n_samples_azimuthal())
+
+        total_hit_count = 0
+        total_hit_coords = []
+
+
+        for phi in angles_azim:
+            hit_count = 0
+            hit_coords = np.array([])
+            # calculate 'effective gamma pulse parameters' ##########
+            eff_height = self.calc_effective_height(
+                r = self.get_gamma_pulse().get_height(),
+                phi = phi,
+                d = self.get_gamma_pulse().get_off_axis_dist(),
+                f = self.get_f()
+            )
+
+            eff_d = self.calc_effective_d(
+                r = self.get_gamma_pulse().get_height(),
+                phi = phi,
+                d = self.get_gamma_pulse().get_off_axis_dist(),
+                f = self.get_f()
+            )
+            hit_count, hit_coords = self.find_hits(eff_height = eff_height, eff_d = eff_d)   
+            
+            if hit_count != 0:
+                total_hit_count += hit_count
+                if len(total_hit_coords) == 0:
+                    total_hit_coords = hit_coords
+                else:
+                    total_hit_coords = np.append(total_hit_coords, hit_coords, axis=0)
+
+
+        return total_hit_count, total_hit_coords
+
+    def calc_effective_height(self, r, phi, d, f):
+        dist = r + f
+
+        d_prime = d - f # new effective d even for 0 deg case
+        #d now defines perpendicular distance to circle centre
+
+        if phi != 0:
+            m = abs( np.tan(phi) ) #gradient of line, direction don't matter cuz symmetric
+            if d * d - ( 1 + m * m) * ( d * d - r * r ) < 0:
+                pass # ignore not intersecting
+            else:
+                x1 = ( d + np.sqrt( d * d 
+                                        - ( 1 + m * m) * ( d * d - r * r ))
+                                        ) / ( 1 + m * m )
+                y1 = m * x1
+
+                x2 = ( d - np.sqrt( d * d 
+                                        - ( 1 + m * m) * ( d * d - r * r ))
+                                        ) / ( 1 + m * m )
+                if x2 < d_prime:
+                    x2 = d_prime
+                else:
+                    d_prime = x2
+
+                y2 = m * x2
+
+                dist = np.sqrt( ( x1 - x2 ) ** 2 + ( y1 - y2 ) ** 2 )
+        
+        return dist
+    
+    def calc_effective_d(self, r, phi, d, f):
+        d_prime = d - f
+        m = abs( np.tan(phi) )
+        x2 = ( d - np.sqrt( d * d 
+                                - ( 1 + m * m) * ( d * d - r * r ))
+                                ) / ( 1 + m * m )
+        if x2 < d_prime:
+            eff_d = d_prime * np.sqrt(
+                1 + np.tan(phi)
+            )
+        else:
+            eff_d = np.sqrt(
+                x2 ** 2 + m * x2
+            )
+        
+        return eff_d
+    
+    def est_npairs(self, angles):
+        """Estimates the number of positron pairs produced
+        and lands on the CsI detector
+
+        Returns:
+            angles: list of angles for each hit
+        """
+        import values as values
+        from cross_section import c_BW
+        from data_read.spectral_data import xray_spectra, gamma_spectra
+        
+        # calculate cross section of each hit and sum #####################################
+        xray_data = xray_spectra('Final_simulation\\data_read\\data\\XrayBath\\XraySpectra\\', resolution=0.5)
+        gamma_data = gamma_spectra('Final_simulation/data_read/data/GammaSpectra/Fig4b_GammaSpecLineouts.mat')
+        xray_energy_sample = xray_data.sample_pdf(
+            min_energy = values.xray_spectra_min,
+            max_energy = values.xray_spectra_max,
+            n = len(angles)
+        )
+
+        gamma_energy_sample = gamma_data.sample_pdf(n_samples = len(angles))
+
+        cs_list = []
+        for i, angle in enumerate(angles):
+            #get cross section
+            # s = 2 * ( 1 - np.cos(angle) ) * 230 * 1.38e-3
+            s = 2 * ( 1 - np.cos(angle) ) * gamma_energy_sample[i] * xray_energy_sample[i]/1e6
+            cs = c_BW(np.sqrt(s)) #get cross sec for 230MeV root s
+            cs *= 1e-28 #convert from barns to m^2
+            cs_list.append(cs)
+
+        # get maximum azimuthal angle we calculate ######################################
+        max_angle = np.arctan( self.get_gamma_pulse().get_height() / 
+                        self.get_gamma_pulse().get_off_axis_dist() )
+        
+        # estimate number of positrons #################################################
+        N_pos = ( values.xray_number_density * values.gamma_photons_number
+                  * values.AMS_transmision * sum(cs_list) 
+                  / self.get_n_samples_azimuthal()
+                  / self.get_n_samples_angular()
+                  / self.get_n_samples()
+                  * ( max_angle / np.pi ) )
+        
+        # estimate uncertainty ##########################################################
+        uncertainty = np.sqrt(
+        (values.gamma_photons_number_err/values.gamma_photons_number) ** 2
+        + (values.xray_number_density_err/values.xray_number_density) ** 2
+        + (values.gamma_length_err / values.gamma_length) ** 2
+        ) * N_pos
+        
+        return np.array([N_pos, uncertainty])
+
+    def plot_hit_count(self, min_delay, max_delay, samples=50, **kwargs):
+        """Plots the hit count and estimated number of pairs for a
+        range of delays
+
+        Args:
+            min_delay (float): Minumum pulse delay (ps)
+            max_delay (float): Maximum pulse delay (ps)
+            samples (int, optional): Number of delays to check. Defaults to 50.
+            **kwargs: optional
+                show_exp_value (bool, optional): Whether to plot the delay used in 2018. Defaults to False.
+                save_data (bool, optional): Whether to save the plot data to a csv. Defaults to False
+                show_progress_bar (bool, optional): Whether to print the progress bar. Defaults to True
+        """
+        # kwaargs ##########################################################
+        show_exp_value = kwargs.get('show_exp_value', False)
+        save_data = kwargs.get('save_data', False)
+        save_data_filename = kwargs.get('save_data_filename', 'Npos_plot_data')
+        show_progress_bar = kwargs.get('show_progress_bar', True)
+
+        # set up figure ############################################
+        fig, ax = plt.subplots() #pylint: disable=unused-variable
+        ax.set_title('Hit count against time delay')
+        ax.set_xlabel('Delay (ps)')
+        ax.set_ylabel('Number of hits')
+
+        twin = ax.twinx()
+        twin.set_ylabel('Number of positrons/pC incident on CsI')
+
+
+        # generate values #########################################
+        delay_list = np.linspace(min_delay, max_delay, samples)
+
+        N_pos_list = []
+        hit_count_list = []
+
+        # progress bar #########################
+        if show_progress_bar:
+            prog = 0
+            print('#' * prog + '-' * ( len(delay_list) - prog ) + f'{ prog / len(delay_list) * 100 :.1f}%')
+
+        for delay in delay_list:
+            hit_count, hit_coords = self.count_hits(delay)
+            N_pos_list.append( self.est_npairs(angles = hit_coords[:, 3]) )
+            hit_count_list.append(hit_count)
+
+            self.xray_bath.resample() #resample x-ray distribution
+            
+            # progress bar updates #####################################
+            if show_progress_bar:
+                prog += 1
+                print('#' * prog + '-' * ( len(delay_list) - prog ) + f'{ prog / len(delay_list) * 100 :.1f}%')
+        N_pos_list = np.array(N_pos_list)
+
+        
+        # plot values ##################################################
+        hits, = ax.plot(delay_list, hit_count_list, '-x', 
+                        label='Hit count', color='red')
+
+        positrons, = twin.plot(delay_list, N_pos_list[:,0,0], '-o', 
+                               label='Number of positrons/pC incident on CsI', color='blue')
+        fill_band = twin.fill_between(delay_list, N_pos_list[:,0,0] - N_pos_list[:,1,0], N_pos_list[:,0,0] + N_pos_list[:,1,0], 
+                                      color='blue', alpha=0.3, label='Uncertainty')
+
+        if show_exp_value:
+            import values as values
+            exp_value = ax.axvline(x = values.delay_experiment,
+                                    ymin = 0, ymax = 1,
+                                    label = 'Delay used in 2018', color = 'orange')
+            ax.legend(handles=[hits, positrons, fill_band, exp_value])
+        else:
+            ax.legend(handles=[hits, positrons, fill_band])
+        ax.grid()
+
+
+        if save_data:
+            import pickle
+            data = {
+                'delay' : delay_list,
+                'hit_count': hit_count_list,
+                'Npos_CsI': N_pos_list[:,0,0],
+                'Npos_CsI_err': N_pos_list[:,1,0]
+            }
+
+            with open(f'{save_data_filename}.pickle', 'wb') as f:
+                pickle.dump(data, f)
+
+        plt.show()
+
+
+    def plot_ang_dist(self, delay):
+        """Plots the angular distribution of hits for
+        a pulse delay
+
+        Args:
+            delay (float): delay between gamma pulse and Xray ignition (ps)
+        """
+        _, hit_coords = self.count_hits(delay)
+        angles = hit_coords[:,3]
+        #angles = self.get_xray_bath().get_xray_coords()[:,2]
+
+        plt.title('Angular distribution of hits')
+        plt.xlabel('Angle')
+        plt.ylabel('Quantity')
+        plt.hist(angles, bins=50)
+        plt.show()
+        
+
+    ############ ACCESS METHODS ##############################################################################
+    def get_n_samples_azimuthal(self):
+        """Access method for n_samples_azimuthal
+
+        Returns:
+            int: number of azimuthal samples to take
+        """
+        return self.n_samples_azimuthal
+    
+    def get_f(self):
+        return self.f
+    
+
+class Test:
+    """
+    For running tests on the hit counter
+    """
+    def __init__(self):
+        pass
+
+    def test_hit_counter(self):
+        """
+        Runs hit counter on experimental values
+        """
+        import values as values
+        from simulation import Xray, Gamma
+        xray = Xray(
+            FWHM = values.xray_FWHM,
+            rotation = 40 * np.pi / 180
+        )
+
+        gamma = Gamma(
+            x_pos = -10e-12 * 3e8 * 1e3,
+            pulse_length = values.gamma_length,
+            height = values.gamma_radius, 
+            off_axis_dist = values.off_axial_dist
+        )
+
+        counter = Hit_counter(
+            xray_bath = xray,
+            gamma_pulse = gamma,
+            f = 0.6,
+            n_samples_azimuthal = 10
+        )
+
+        counter.plot_hit_count(
+            min_delay = -10,
+            max_delay = 500,
+            samples = 50,
+            show_exp_value = True,
+            save_data = True
+        )
+
+if __name__ == '__main__':
+    test = Test()
+    test.test_hit_counter()
---
+++ Optimise_f.py
@@ -0,0 +1,55 @@
+"""
+Optimises the parameter f
+f is the distance of the tungsten block shielding to the centre
+of the gamma profile
+Basically how 'non-semicircle' it becomes
+
+Timothy Chew
+22/07/25
+"""
+"""
+Check simulation over different input angles and see for differences in maximum yield
+
+Timothy Chew
+22/07/25
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('f_optimise.csv', delimiter=',', skiprows=1)
+
+f = data[:,0]
+npos = data[:,1]
+npos_err = data[:,2]
+
+fig, ax = plt.subplots() #pylint: disable=unused-variable
+ax.set_title('Positron count vs shielding')
+ax.set_xlabel('f /mm')
+ax.set_ylabel('Maximum number of positrons/pC incident on CsI')
+
+ax.plot(
+    f, npos,
+    '-o',
+    label = 'Positron yield',
+    color = 'blue'
+)
+
+ax.fill_between(
+    x = f,
+    y1 = npos - npos_err,
+    y2 = npos + npos_err,
+    label = 'Uncertainty',
+    color = 'blue',
+    alpha = 0.3
+)
+
+ax.axvline(x = 0,
+    ymin = 0, ymax = 1,
+    label = 'f used in 2018', color = 'orange')
+
+ax.set_axisbelow(True)
+ax.grid()
+ax.legend()
+
+plt.show()
