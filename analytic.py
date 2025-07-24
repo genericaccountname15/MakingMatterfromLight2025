d9a7c21afff526c51251a0a5afb22af604f199c1
timothy <Timothy.chew22@imperial.ac.uk>
2025-07-24 14:25:17 +0100

    Fixed some errors

--- analytic.py
+++ analytic.py
@@ -12,16 +12,14 @@ import Final_simulation.values as values
 
 def func(t, n, d, beam_radius):
     #unit conversion
-    t_delay = t *1e-12
+    t_delay = (t) *1e-12
     d *= 1e-3
     beam_radius *= 1e-3
     N = n * 2/3 * np.pi * 1e-3 ** 3
-    t0 = 4e-11
-    t0 = t_delay + (d + beam_radius ) ** 2 / ( values.c ** 2 * t_delay)
-    t0 = 2e-11
+    t0 = (d + beam_radius ) ** 2 / ( values.c ** 2 * t_delay)
 
 
-    hits = N / (2 * np.pi * (values.c * t_delay ** 2) ) * (
+    hits = N / (np.pi * (values.c * t_delay) ) * (
         np.arcsin( (d + beam_radius) / ( values.c * ( t_delay + t0 ) )
         - np.arcsin( d / ( values.c * t_delay ) ) )
     )
@@ -31,6 +29,9 @@ tmin =  values.off_axial_dist / values.c * 1e-3 * 1e12
 t = np.linspace(tmin, 200, 1000) #time delay (ps)
 Npairs = func( t, values.xray_number_density, values.off_axial_dist, values.gamma_radius )
 
-plt.ylim(-1e23, 3e24)
+plt.title('Analytical time delay hit count relation')
+plt.xlabel('time delay')
+plt.ylabel('hit count (arbritary units)')
+# plt.ylim(-1e13, 3e13)
 plt.plot(t, Npairs, 100)
 plt.show()
