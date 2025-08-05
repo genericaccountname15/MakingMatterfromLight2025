92e8e37a012d2a919c051a21bc9d348173adbd35
timothy <Timothy.chew22@imperial.ac.uk>
2025-08-05 11:32:08 +0100

    Attempt at bug fix

--- simulation_restructure/run_sim_lambertian_angle_Pd.py
+++ simulation_restructure/run_sim_lambertian_angle_Pd.py
@@ -9,14 +9,14 @@ from theory import values   #pylint: disable=import-error
 
 
 run_data_collection(
-    variables = np.linspace(0, 90, 10) * np.pi / 180,
+    variables = np.array([1.3962634, 1.57079633]),
     variable_name = 'angle',
     variable_parameter_name = 'rotation',
     units = 'radians',
     old_value = values.source_angle * np.pi / 180,
     xray_type = 'lambertian_Pd',
     repeat = 3,
-    additional_label = 'Pd_4Aug',
+    additional_label = 'Pd_4Aug_part3',
     sim_params = accurate,
     sample_params = deep
     )
