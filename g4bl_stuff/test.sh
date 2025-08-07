958c39b1c0624db7f14566a895ec6a5dabc740e3
timothy <Timothy.chew22@imperial.ac.uk>
2025-08-07 11:50:29 +0100

    Gamma source

--- g4bl_stuff/test.sh
+++ g4bl_stuff/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source "/cygdrive/c/Program Files/Muons, Inc/G4beamline/bin/g4bl-setup.sh"
-g4bl "C:\Users\Timothy Chew\Desktop\UROP2025\MakingMatterfromLight2025\g4bl_stuff\g4beamlinesfiles\test.g4bl" viewer=best
+g4bl "C:\Users\Timothy Chew\Desktop\UROP2025\MakingMatterfromLight2025\g4bl_stuff\g4beamlinesfiles\Gammasource.g4bl" viewer=best
 
