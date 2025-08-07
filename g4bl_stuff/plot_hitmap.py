b69e9431ad2b66fb1a7829630c20946e10a57762
timothy <Timothy.chew22@imperial.ac.uk>
2025-08-07 14:39:15 +0100

    Setup and datafiles

---
+++ g4bl_stuff/plot_hitmap.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.loadtxt('g4bl_stuff/g4beamlinesfiles/gamma_profile_Det.txt', skiprows=1)
+
+x = np.arctan2(data[:,1],85+50+45+100+25+400+160) * 1000
+y = np.arctan2(data[:,0],85+50+45+100+25+400+160) * 1000
+
+plt.title('hit map of gamma beam source at the interaction point')
+plt.xlabel('y/mrad')
+plt.ylabel('x/mrad')
+plt.hist2d(x, y, bins=500, cmap='gnuplot')
+plt.colorbar(label='Counts (AU)')
+plt.xlim(-6,4)
+plt.ylim(-5,5)
+plt.show()
