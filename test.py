c8f64dceb1930e92a90635c26e5494a3ce5f6284
timothy <Timothy.chew22@imperial.ac.uk>
2025-08-11 12:47:57 +0100

    Test file removal

--- test.py
+++
@@ -1,6 +0,0 @@
-import uproot
-
-file = uproot.open("g4beamline.root")
-tree = file["VirtualDetector/noise_measure_Det;1"]  # usually the tree inside
-df = tree.arrays(library="pd")
-print(df.head())
