8b74323f4dda4ab67a92c7d814577a30ae0ad986
timothy <Timothy.chew22@imperial.ac.uk>
2025-08-13 16:56:11 +0100

    Removed param definition in g4beamline files

--- noise_simulation/analysis/setup_analysis.py
+++ noise_simulation/analysis/setup_analysis.py
@@ -63,7 +63,8 @@ class Analysis:
             f'd={d}',
             f'angle={angle}',
             f'sin={np.sin(angle)}',
-            f'cos={np.cos(angle)}'
+            f'cos={np.cos(angle)}',
+            f'filename={self.get_file_dict()['output fname']}'
         ]
 
         status = subprocess.run(command, check=True, cwd=self.get_file_dict()['workspace dir'])
