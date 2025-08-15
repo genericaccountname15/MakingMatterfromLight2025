6a6f11d80154b4ec73c703a108d334cabcde497e
timothy <Timothy.chew22@imperial.ac.uk>
2025-08-15 12:25:15 +0100

    Made it a new file so the f simulation doesn't drag

--- noise_simulation/measure_noise_angle.py
+++ noise_simulation/measure_noise_angle.py
@@ -8,7 +8,7 @@ from analysis.file_locations import files_HPC
 if __name__ == '__main__':
     analysis = Analysis(file_dict = files_HPC, save_filename = 'noise_angle_gamma_Det.txt')
     analysis.run_analysis(
-        g4bl_filename = 'noise_simulation/g4beamlinefiles/whole_setup_gammaspectra.g4bl',
+        g4bl_filename = 'noise_simulation/g4beamlinefiles/whole_setup_gammaspectra_10x.g4bl',
         save_filedir = 'angle_noise_measure_15Aug',
         variables = np.linspace(0, 90, 10),
         variable_name = 'angle',
--- noise_simulation/measure_noise_d.py
+++ noise_simulation/measure_noise_d.py
@@ -8,7 +8,7 @@ from analysis.file_locations import files_HPC
 if __name__ == '__main__':
     analysis = Analysis(file_dict = files_HPC, save_filename = 'noise_d_gamma_Det.txt')
     analysis.run_analysis(
-        g4bl_filename = 'noise_simulation/g4beamlinefiles/whole_setup_gammaspectra.g4bl',
+        g4bl_filename = 'noise_simulation/g4beamlinefiles/whole_setup_gammaspectra_10x.g4bl',
         save_filedir = 'd_noise_measure_15Aug',
         variables = np.linspace(0.1, 3.0, 10),
         variable_name = 'd',
